6d184a49822cf99dda94922e2fd81216ad439473
John Baldwin <jhb@FreeBSD.org>
2017-10-30 04:27:58 -0700

    Move MIPS ABI constants under a single #ifdef __mips__.

--- include/__libunwind_config.h
+++ include/__libunwind_config.h
@@ -64,15 +64,18 @@
 #  define _LIBUNWIND_CONTEXT_SIZE 16
 #  define _LIBUNWIND_CURSOR_SIZE 24
 #  define _LIBUNWIND_HIGHEST_DWARF_REGISTER _LIBUNWIND_HIGHEST_DWARF_REGISTER_OR1K
-# elif defined(__mips__) && defined(_ABIO32) && defined(__mips_soft_float)
-#  define _LIBUNWIND_TARGET_MIPS_O32 1
-#  define _LIBUNWIND_CONTEXT_SIZE 18
-#  define _LIBUNWIND_CURSOR_SIZE 29
-#  define _LIBUNWIND_HIGHEST_DWARF_REGISTER _LIBUNWIND_HIGHEST_DWARF_REGISTER_MIPS
-# elif defined(__mips__) && defined(_ABI64) && defined(__mips_soft_float)
-#  define _LIBUNWIND_TARGET_MIPS_N64 1
-#  define _LIBUNWIND_CONTEXT_SIZE 35
-#  define _LIBUNWIND_CURSOR_SIZE 47
+# elif defined(__mips__)
+#  if defined(_ABIO32) && defined(__mips_soft_float)
+#    define _LIBUNWIND_TARGET_MIPS_O32 1
+#    define _LIBUNWIND_CONTEXT_SIZE 18
+#    define _LIBUNWIND_CURSOR_SIZE 29
+#  elif defined(_ABI64) && defined(__mips_soft_float)
+#    define _LIBUNWIND_TARGET_MIPS_N64 1
+#    define _LIBUNWIND_CONTEXT_SIZE 35
+#    define _LIBUNWIND_CURSOR_SIZE 47
+#  else
+#    error "Unsupported MIPS ABI and/or environment"
+#  endif
 #  define _LIBUNWIND_HIGHEST_DWARF_REGISTER _LIBUNWIND_HIGHEST_DWARF_REGISTER_MIPS
 # else
 #  error "Unsupported architecture."
